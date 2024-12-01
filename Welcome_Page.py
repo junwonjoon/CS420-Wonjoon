cb33455c1c7f23e2dedcfd76818aa74e240b0e36
Wonjoon <junwonjoon41@gmail.com>
2024-12-01 14:04:59 -0700

    Update Welcome_Page.py

--- Welcome_Page.py
+++ Welcome_Page.py
@@ -11,11 +11,12 @@ st.markdown(
 # Welcome to My Interactive Demonstration of Schedulers
 
 ## Overview
-This project provides an engaging, hands-on demonstration of various CPU scheduling algorithms, crucial for operating systems. Learn how schedulers determine the order in which processes execute to maximize efficiency and fairness.
+This project provides an engaging, hands-on demonstration of various CPU scheduling algorithms, crucial for operating systems. Learn how schedulers determine the order in which processes execute to maximize efficiency and fairness. 
 
 ## Topics Covered
 - **[Round Robin (RR)](https://cs420-scheduler-demo.streamlit.app/Round_Robin_Scheduling):** Time slices are shared equally among processes.
 - **Rate Monotonic Scheduling (RMS):** A real-time scheduling algorithm where processes with shorter periods are assigned higher priorities.
+To navigate please use the side bar on the left to view different scheduling algorithms.
 
 ## Interactive Features
 - Visualize the scheduling process in real-time.
